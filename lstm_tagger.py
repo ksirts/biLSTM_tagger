add7b93d0f2f47fde7c681ee820372336fa7b654
Kairit Sirts <kairit.sirts@mail.com>
2018-11-19 18:44:44 +0200

    packed char sequences

--- lstm_tagger.py
+++ lstm_tagger.py
@@ -6,12 +6,15 @@ import numpy as np
 
 import torch
 from torchtext import data
+from torchtext import vocab
 
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
 from torchtext import datasets
+
+from torch.nn.utils.rnn import pack_padded_sequence
 
 SEED = 1234
 
@@ -26,6 +29,9 @@ parser.add_argument('--bi-words', action='store_true', help='Use bidirectional w
 parser.add_argument('--bi-chars', action='store_true', help='Use bidirectional char encoder')
 parser.add_argument('--use-adam', action='store_true', help='Use Adam optimizer')
 parser.add_argument('--pretrained', action='store_true', help='Use pretrained embeddings')
+parser.add_argument('--freeze', action='store_true', help='Freeze pretrained embeddings')
+parser.add_argument('--oov-embeddings', action='store_true', help='Load pretrained embeddings for all words')
+parser.add_argument('--input-projection', action='store_true', help='Use input projection in the word model')
 args = parser.parse_args()
 
 print('# Load from file:\t\t', args.load, file=sys.stderr, flush=True)
@@ -34,17 +40,21 @@ print('# BiLSTM for words:\t\t', args.bi_words, file=sys.stderr, flush=True)
 print('# BiLSTM for chars:\t\t', args.bi_chars, file=sys.stderr, flush=True)
 print('# Use Adam optimizer:\t\t', args.use_adam, file=sys.stderr, flush=True)
 print('# Use pretrained embeddings:\t', args.pretrained, file=sys.stderr, flush=True)
+print('# Freeze pretrained embeddings:\t', args.freeze, file=sys.stderr, flush=True)
+print('# Use embeddings for OOV words:\t', args.oov_embeddings, file=sys.stderr, flush=True)
+print('# Use input projection:\t\t', args.input_projection, file=sys.stderr, flush=True)
 
-def sequence_accuracy(scores, targets, lengths):
+def sequence_accuracy(words, scores, targets):
     _, predict = torch.max(scores,1)
-    batch_len = lengths.max()
-    mask = torch.Tensor([[0] + [1 for i in range(l-2)] + [0 for i in range(batch_len - l + 1)] for l in lengths])
-    mask = mask.permute(1, 0).reshape(-1)
-    total = sum(mask)
-    correct = predict == targets
-    correct = correct.type(torch.FloatTensor)
+
+    mask = torch.ones(words.size())
+    mask[words==WORD.vocab.stoi['<bos>']] = 0
+    mask[words==WORD.vocab.stoi['<eos>']] = 0
+    mask[words==WORD.vocab.stoi['<pad>']] = 0
+    total = mask.sum().type(torch.FloatTensor)
+    correct = (predict == targets).type(torch.FloatTensor)
     masked_correct = mask * correct
-    acc = masked_correct.sum() / total
+    acc = masked_correct.sum().type(torch.FloatTensor) / total
     return acc
 
 def oov_accuracy(words, scores, targets):
@@ -57,7 +67,7 @@ def oov_accuracy(words, scores, targets):
         data = np.array(data)
         return np.mean(data[:,0] == data[:,1])
 
-def train(model, iterator, optimizer, criterion, char_model=None):
+def train(model, iterator, optimizer, criterion, char_model=None, oov_embeds=False):
 
     epoch_loss = 0
     epoch_acc = 0
@@ -77,17 +87,23 @@ def train(model, iterator, optimizer, criterion, char_model=None):
         char_embeddings = None
         
         if char_model:
-            chars = batch.char
-            char_embeddings = char_model(chars)
+            chars, _, char_lengths = batch.char
+            char_embeddings = char_model(chars, char_lengths)
+
+        if oov_embeds:
+            words = words.cpu()
+            words = F.embedding(words, WORD.vocab.vectors)
+            words = words.cuda()
 
         predictions = model(words, char_embeddings)
         predictions = predictions.reshape(-1, predictions.size()[-1])
         labels = batch.udtag.reshape(-1)
+        words = batch.word[0].reshape(-1)
 
         # Compute the loss, gradients, and update the parameters by
         #calling optimizer.step()
         loss = criterion(predictions, labels)
-        acc = sequence_accuracy(predictions, labels, lengths)
+        acc = sequence_accuracy(words, predictions, labels)
         
         loss.backward()
         optimizer.step()
@@ -100,7 +116,7 @@ def train(model, iterator, optimizer, criterion, char_model=None):
         
     return epoch_loss / len(iterator), epoch_acc / len(iterator)
 
-def evaluate(model, iterator, criterion, char_model=None):
+def evaluate(model, iterator, criterion, char_model=None, oov_embeds=False):
     
     epoch_loss = 0.0
     epoch_acc = 0.0
@@ -120,21 +136,26 @@ def evaluate(model, iterator, criterion, char_model=None):
             char_embeddings = None
         
             if char_model:
-                chars = batch.char
-                char_embeddings = char_model(chars)
+                chars, _, char_lengths = batch.char
+                char_embeddings = char_model(chars, char_lengths)
+
+            if oov_embeds:
+                words = words.cpu()
+                words = F.embedding(words, WORD.vocab.vectors)
+                words = words.cuda()
 
             predictions = model(words, char_embeddings)
             predictions = predictions.reshape(-1, predictions.size()[-1])
             labels = batch.udtag.reshape(-1)
-            # print(predictions.size(), labels.size())
+            words = batch.word[0].reshape(-1)
             
             loss = criterion(predictions, labels)
-            acc = sequence_accuracy(predictions, labels, lengths)
+            acc = sequence_accuracy(words, predictions, labels)
 
             epoch_loss += loss.item()
             epoch_acc += acc.item()
 
-            words = words.reshape(-1)
+            words = batch.word[0].reshape(-1)
             oov_acc = oov_accuracy(words, predictions, labels)
             if oov_acc:
                 epoch_oov_acc += oov_acc
@@ -151,18 +172,27 @@ class CharEmbeddings(nn.Module):
         self.bidirectional=bidirectional
         
         self.char_embeddings = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, bidirectional=bidirectional)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True, bidirectional=bidirectional)
         
-    def forward(self, chars):
+    def forward(self, chars, lengths):
         chars_size = chars.size()
         # Aggregate sequence length and batch dimensions
-        chars = chars.view(-1, chars_size[-1])
-        # Move batch to second dimension
-        chars = chars.permute(1, 0)
+        chars = chars.reshape(-1, chars_size[-1])
         # Embed characters
         embeds = self.char_embeddings(chars)
-        # Send through LSTM
-        lstm_out, hidden = self.lstm(embeds)
+
+        # Sort and pack the embeddings
+        lengths = lengths.reshape(-1)
+        lengths_sort, idx_sort = torch.sort(lengths, dim=0, descending=True)
+        # Replace 0 entries with 1s
+        lengths_sort[lengths_sort==0] = 1
+        _, idx_unsort = torch.sort(idx_sort, dim=0)
+        embeds_sort = embeds.index_select(0, idx_sort)
+        embeds_pack= pack_padded_sequence(embeds_sort, lengths_sort, batch_first=True)
+
+        # Send the pack through LSTM
+        _, hidden = self.lstm(embeds_pack)
+        
         # Concatenate states to get word embeddings
         word_embeds = torch.cat(hidden, dim=2)
 
@@ -175,24 +205,37 @@ class CharEmbeddings(nn.Module):
             word_embeds = word_embeds.permute(1, 0, 2)
             dim = 4 * self.hidden_dim
         
-        # Reshape back to batch x sequence x dimension
-        word_embeds = word_embeds.reshape(chars_size[:2] + (dim,))
-        # Permute axes to sequence x batch x dimension
-        word_embeds = word_embeds.permute(1, 0, 2)
+        # Reshape back to (batch x sequence) x dimension
+        word_embeds = word_embeds.reshape(-1, dim)
+
+        # Restore the original index ordering
+        word_embeds = word_embeds.index_select(0, idx_unsort)
+        # Reshape back to original shape
+        word_embeds = word_embeds.reshape(chars_size[0], chars_size[1], -1)
         return word_embeds
 
 
 class LSTMTagger(nn.Module):
 
-    def __init__(self, word_embedding_dim, embedding_dim, hidden_dim, vocab_size, tagset_size, bidirectional=False):
+    def __init__(self, word_embedding_dim, embedding_dim, hidden_dim, tagset_size, vocab_size=None, bidirectional=False, freeze=False, input_projection=False):
         super(LSTMTagger, self).__init__()
         self.hidden_dim = hidden_dim
 
-        self.word_embeddings = nn.Embedding(vocab_size, word_embedding_dim)
+        if vocab_size is not None:
+            print('# Creating word embedding layer ...', file=sys.stderr, flush=True)
+            self.word_embeddings = nn.Embedding(vocab_size, word_embedding_dim)
+            if freeze:
+                print('# Freezing word embedding layer ...', file=sys.stderr, flush=True)
+                self.word_embeddings.weight.requires_grad = False
+
+        if input_projection:
+            print('# Creating input projection layer ...', file=sys.stderr, flush=True)
+            self.embed2input = nn.Linear(word_embedding_dim, word_embedding_dim)
+
 
         # The LSTM takes word embeddings as inputs, and outputs hidden states
         # with dimensionality hidden_dim.
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, bidirectional=bidirectional)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True, bidirectional=bidirectional)
 
         # The linear layer that maps from hidden state space to tag space
         linear_in = hidden_dim
@@ -201,20 +244,25 @@ class LSTMTagger(nn.Module):
         self.hidden2tag = nn.Linear(linear_in, tagset_size)
 
 
-    def forward(self, sentence, char_embeds=None):
-        embeds = self.word_embeddings(sentence)
+    def forward(self, words, char_embeds=None):
+        if hasattr(self, 'word_embeddings'):
+            words = self.word_embeddings(words)
+
+        if hasattr(self, 'embed2input'):
+            words = self.embed2input(words)
+        
         if char_embeds is not None:
-            embeds = torch.cat([embeds, char_embeds], dim=2)
-        lstm_out, hidden = self.lstm(embeds)
+            embeds = torch.cat([words, char_embeds], dim=2)
+        lstm_out, _ = self.lstm(embeds)
         tag_space = self.hidden2tag(lstm_out)
         return tag_space
 
-WORD = data.Field(init_token="<bos>", eos_token="<eos>", include_lengths=True)
-UD_TAG = data.Field(init_token="<bos>", eos_token="<eos>", unk_token=None)
+WORD = data.Field(init_token="<bos>", eos_token="<eos>", include_lengths=True, lower=True, batch_first=True)
+UD_TAG = data.Field(init_token="<bos>", eos_token="<eos>", unk_token=None, batch_first=True)
 
 if args.chars:
     CHAR_NESTING = data.Field(tokenize=list, init_token="<bos>", eos_token="<eos>")
-    CHAR = data.NestedField(CHAR_NESTING, init_token="<bos>", eos_token="<eos>")
+    CHAR = data.NestedField(CHAR_NESTING, init_token="<bos>", eos_token="<eos>", include_lengths=True)
     fields = ((('word', 'char'), (WORD, CHAR)), ('udtag', UD_TAG))
 else:
     fields = (('word', WORD), ('udtag', UD_TAG))
@@ -222,10 +270,16 @@ print('# fields =', fields, file=sys.stderr, flush=True)
 
 train_data, valid_data, test_data = datasets.UDPOS.splits(fields=fields)
 
-vectors=None
+WORD.build_vocab(train_data)
+
 if args.pretrained:
-    vectors='glove.6B.300d'
-WORD.build_vocab(train_data, vectors=vectors)
+    vectors=vocab.GloVe(name="6B", dim=300)
+
+    if args.oov_embeddings:
+        WORD.vocab.extend(vectors)
+    WORD.vocab.load_vectors(vectors)
+    
+
 print('# Word vocab size:\t', len(WORD.vocab), file=sys.stderr, flush=True)
 UD_TAG.build_vocab(train_data)
 print('# Tag vocab size:\t', len(UD_TAG.vocab), file=sys.stderr, flush=True)
@@ -269,7 +323,11 @@ if args.chars:
     char_model = CharEmbeddings(CHAR_EMBEDDING_DIM, CHAR_HIDDEN_DIM, len(CHAR.vocab), bidirectional=args.bi_chars)
 
 print('# Creating word model ...', file=sys.stderr, flush=True)
-model = LSTMTagger(WORD_EMBEDDING_DIM, EMBEDDING_DIM, HIDDEN_DIM, len(WORD.vocab), len(UD_TAG.vocab), bidirectional=args.bi_words)
+vocab_len = len(WORD.vocab)
+if args.oov_embeddings:
+    vocab_len = None
+model = LSTMTagger(WORD_EMBEDDING_DIM, EMBEDDING_DIM, HIDDEN_DIM, len(UD_TAG.vocab), vocab_size=vocab_len, bidirectional=args.bi_words, freeze=args.freeze, 
+                   input_projection=args.input_projection)
 
 if args.load:
     print('# Loading model from file ...', file=sys.stderr, flush=True)
@@ -281,26 +339,30 @@ else:
     print('# Creating new parameters ...', file=sys.stderr, flush=True)
 
 
-if args.pretrained:
+if args.pretrained and not args.oov_embeddings:
     print('# Copying pretrained embeddings ...', file=sys.stderr, flush=True)
     pretrained_embeddings = WORD.vocab.vectors
     model.word_embeddings.weight.data.copy_(pretrained_embeddings)
 
+print('# Creating loss function ...', file=sys.stderr, flush=True)
 loss_function = nn.CrossEntropyLoss(ignore_index=WORD.vocab.stoi['<pad>'])
 
-params = model.parameters()
+print('# Creating the parameter optimizer ...', file=sys.stderr, flush=True)
+params = list(filter(lambda p: p.requires_grad, model.parameters()))
 if args.chars:
-    params = list(char_model.parameters()) + list(model.parameters())
+    params += list(char_model.parameters())
 
 if args.use_adam:
     optimizer = optim.Adam(params)
 else:
     optimizer = optim.SGD(params, lr=0.1)
 
+print('# Copying objects to device ...', file=sys.stderr, flush=True)
 model = model.to(device)
 if args.chars:
     char_model.to(device)
 loss_function = loss_function.to(device)
+
 
 N_EPOCHS = 400
 LIMIT = 40
@@ -309,12 +371,13 @@ best_epoch = 0
 best_loss = 0
 best_acc = 0
 
+print('# Start training ...', file=sys.stderr, flush=True)
 for epoch in range(N_EPOCHS):
     if epoch - best_epoch > LIMIT:
         break
 
-    train_loss, train_acc = train(model, train_iterator, optimizer, loss_function, char_model)
-    valid_loss, valid_acc, valid_oov_acc = evaluate(model, valid_iterator, loss_function, char_model)
+    train_loss, train_acc = train(model, train_iterator, optimizer, loss_function, char_model, args.oov_embeddings)
+    valid_loss, valid_acc, valid_oov_acc = evaluate(model, valid_iterator, loss_function, char_model, args.oov_embeddings)
     if valid_acc > best_acc:
         print(f'Epoch {epoch+1:01}: saving the best model ...', file=sys.stderr, flush=True)
         best_acc = valid_acc
@@ -332,6 +395,6 @@ print(f'Best epoch: {best_epoch+1:02}, Best Acc: {best_acc*100:.2f}%', file=sys.
 model.load_state_dict(torch.load('.models/best_model'))
 if args.chars:
     char_model.load_state_dict(torch.load('.models/best_char_model'))
-t_loss, t_acc, t_oov_acc  = evaluate(model, test_iterator, loss_function, char_model)
+t_loss, t_acc, t_oov_acc  = evaluate(model, test_iterator, loss_function, char_model, args.oov_embeddings)
 print(f'Best epoch: {best_epoch+1:02}, Dev loss: {best_loss:.3f}, Dev acc: {best_acc*100:.2f}%, Test loss: {t_loss:.3f}, Test acc: {t_acc*100:.2f}%, Test OOV acc: {t_oov_acc*100:.2f}%', 
       file=sys.stdout, flush=True)
