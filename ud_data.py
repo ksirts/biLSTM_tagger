5af3bb417ce2a225cbbc0addeb408e2c839cda15
Kairit Sirts <kairit.sirts@gmail.com>
2019-01-29 13:42:27 +0200

    Load morph data

--- ud_data.py
+++ ud_data.py
@@ -5,6 +5,8 @@ from torchtext import data
 lang_map = {'en': 'English',
             'et': 'Estonian'}
 
+col_list = []
+
 
 class SequenceTaggingDataset(data.Dataset):
     """Defines a dataset for sequence tagging. Examples in this dataset
@@ -47,6 +49,64 @@ class SequenceTaggingDataset(data.Dataset):
             if columns:
                 examples.append(data.Example.fromlist(columns, fields))
         super(SequenceTaggingDataset, self).__init__(examples, fields)
+
+
+class UDPOS(SequenceTaggingDataset):
+
+    # Universal Dependencies dataset
+
+    @classmethod
+    def splits(cls, path=None, root='.data', train=None, validation=None, test=None, **kwargs):
+        cls.name = 'ud-treebanks-v2.1'
+        print(kwargs)
+        cls.dirname = 'UD_{}'.format(lang_map[kwargs['lang']])
+       #  print(root, cls.name, cls.dirname)
+
+        path = os.path.join(root, cls.name, cls.dirname)
+
+        return super(UDPOS, cls).splits(path=path, root=root, train=train,
+                                             validation=validation,
+                                             test=test,
+                                             fields=kwargs['fields'], logger=kwargs['logger'])
+
+
+class MorphTaggingDataset(data.Dataset):
+    """Defines a dataset morphological tagging read from
+    a CONLLU file. The POS and morph fields must be concatenated.
+    """
+
+    @staticmethod
+    def sort_key(example):
+        for attr in dir(example):
+            if not callable(getattr(example, attr)) and \
+                    not attr.startswith("__"):
+                return len(getattr(example, attr))
+        return 0
+
+    def __init__(self, path, fields, logger, separator='\t'):
+        examples = []
+        columns = {}
+        logger.info('input path: {}'.format(path))
+        with open(path) as input_file:
+            for line in input_file:
+                line = line.strip()
+                # This condition was added to torchtext SequenceTaggingDataset class
+                # to account for comment lines in input files
+                if line.startswith('#'):
+                    continue
+                elif line == "":
+                    if columns:
+                        examples.append(data.Example.fromdict(columns, fields))
+                    columns = {}
+                else:
+                    for i, column in enumerate(line.split(separator)):
+                        if len(columns) < i + 1:
+                            columns.append([])
+                        columns[i].append(column)
+
+            if columns:
+                examples.append(data.Example.fromlist(columns, fields))
+        super(MorphTaggingDataset, self).__init__(examples, fields)
 
 
 class UDPOSMorph(SequenceTaggingDataset):
